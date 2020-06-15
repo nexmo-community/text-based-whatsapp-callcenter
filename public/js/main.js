07adb45edfc1517827256b7bc91d70c9b70c23ad
Martyn Davies <martynrdavies@gmail.com>
2020-06-15 12:22:29 +0200

    added validation for agent fields

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,43 @@
-$.get('/getAgents', function(data){
-    data.forEach(element => {
-      var rowHtml = "<tr><td>"+element['name']+"</td><td>"+element['number']+"</td><td>"+element['availability']+"</td></tr>";
-      $("#agentTable tbody").append(rowHtml);
-    });    
-})
-$.get('/getCustomers', function(data){
-    data.forEach(element => {
-      var rowHtml = "<tr><td>"+element['assignedAgentNum']+"</td><td>"+element['customerNumber']+"</td><td>"+element['emoji']+"</td></tr>";
-      $("#customersTable tbody").append(rowHtml);
-    });    
-})
+// Elements
+const addAgentButton = document.getElementById('addAgentButton');
+const agentNumberField = document.getElementById('agentNum');
+const agentNameField = document.getElementById('agentName');
+const errorField = document.getElementsByClassName('error');
+
+// Hide error notice on load
+$('.error').hide();
+
+// Ensure that the Agent fields are not empty
+addAgentButton.addEventListener('click', (e) => {
+  if (agentNameField.value == '') {
+    e.preventDefault();
+    console.log('blank');
+  }
+});
+
+$.get('/getAgents', function (data) {
+  data.forEach((element) => {
+    var rowHtml =
+      '<tr><td>' +
+      element['name'] +
+      '</td><td>' +
+      element['number'] +
+      '</td><td>' +
+      element['availability'] +
+      '</td></tr>';
+    $('#agentTable tbody').append(rowHtml);
+  });
+});
+$.get('/getCustomers', function (data) {
+  data.forEach((element) => {
+    var rowHtml =
+      '<tr><td>' +
+      element['assignedAgentNum'] +
+      '</td><td>' +
+      element['customerNumber'] +
+      '</td><td>' +
+      element['emoji'] +
+      '</td></tr>';
+    $('#customersTable tbody').append(rowHtml);
+  });
+});
